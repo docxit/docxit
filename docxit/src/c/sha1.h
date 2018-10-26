6a260b532b3d2c70e42b3e149677aa53c1958747
2390363806 <2390363806@qq.com>
2018-10-26 18:49:25 +0800

    sha1

--- docxit/src/c/sha1.h
+++ docxit/src/c/sha1.h
@@ -1,6 +1,14 @@
 #ifndef __SHA1_H__
 #define __SHA1_H__
 
-string valueSHA_1(char* filename);
+#include<iostream>
+#include<string>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<unistd.h>
+#include<fcntl.h>
+using namespace std;
+
+string valueSHA_1(string filename);
 
 #endif
