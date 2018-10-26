8c19b4b516300e912d5ce76cf44d6e09317dff1b
2390363806 <2390363806@qq.com>
2018-10-26 19:42:17 +0800

    .docxitPath

--- docxit/src/c/init.c
+++ docxit/src/c/init.c
@@ -8,7 +8,7 @@ void docxitInit()
   system("mkdir $PWD/.docxit/object");//create folder objects to hold all history version files
 	system("touch $PWD/.docxit/HEAD");//create file HEAD to hold current version pointer
 	system("touch $PWD/.docxit/index");//create file index to be the stage
- system("echo $PWD/ >> ~/.docxitinfo");
+ system("echo $PWD/ >> ~/.docxitPath");
 
 }
 
