153b73f119da4a32d5e674626222f9a494693dc2
2390363806 <2390363806@qq.com>
2018-10-27 14:12:44 +0800

    shellCommand getcwd

---
+++ docxit/src/c/blobCreate.h
@@ -0,0 +1,12 @@
+#ifndef __BLOB_CREATE_H__
+#define __BLOB_CREATE_H__
+
+#include<iostream>
+#include<string>
+#include<stdlib.h>
+
+using namespace std;
+
+void blobCreate(string filename, string value_sha1);
+
+#endif//__BLOB_CREATE_H__
