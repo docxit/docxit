cf8bcd107ea7ef23ca877ab31b3717e8642cbcf6
yxyyyxxyy <yixinyu@mail.ustc.edu.cn>
2018-11-06 14:19:21 +0800

    add some instructions and modify parser

--- docxit/src/c/docxit.cpp
+++ docxit/src/c/docxit.cpp
@@ -6,6 +6,15 @@
 
 #define EXE_DIR "/usr/local/lib/docxit/"
 
+static void checkDocxitPath(char *argv[]){
+    int isrepo = docxitPath();
+	if(isrepo == 0 || isrepo == 2){
+        printf("fatal: not a docxit repository\n");
+        exit(0);
+    }
+    argv[0] = (char *)DOCXIT_PATH.c_str();
+}
+
 int main(int argc, char *argv[])
 {
     //TODO: alias
@@ -21,64 +30,247 @@ Usage: docxit [--version] [--help] <command> [<args>]\n\
         exit(0);
 	}
 
-    if(!strcmp(argv[1], "init")){
-        if(docxitPath())
-            printf("Nested initialization not supported\n");
-        else if(execv(EXE_DIR"init", argv) == -1)
-            perror(EXE_DIR"init");
-        return 0;
-	}
+	char *cur = argv[1];
+	switch(*cur ++){
+        case 'a':
+            if(!strcmp(cur, "dd")){
+                checkDocxitPath(argv);
+                if(execv(EXE_DIR"add", argv) == -1){
+                    perror(EXE_DIR"add");
+                    exit(0);
+                }
+            }
+            else{
+                printf("unknown option: %s\n%s", argv[1], usage);
+                printf("the most similar command is %s\n", "add");
+                exit(0);
+            }
 
-	int isrepo = docxitPath();
-	if(isrepo == 0 || isrepo == 2){
-        printf("fatal: not a docxit repository\n");
-        return 0;
-    }
-    argv[0] = (char *)DOCXIT_PATH.c_str();
+        case 'b':
+            if(!strcmp(cur, "ranch")){
+                checkDocxitPath(argv);
+                if(execv(EXE_DIR"branch", argv) == -1){
+                    perror(EXE_DIR"branch");
+                    exit(0);
+                }
+            }
+            else{
+                printf("unknown option: %s\n%s", argv[1], usage);
+                printf("the most similar command is %s\n", "branch");
+                exit(0);
+            }
 
-	if(!strcmp(argv[1], "--help")){
-        printf("%s", usage);
-	}
-	else if(!strcmp(argv[1], "--version")){
-        printf("%s", version);
-	}
-	else if(!strcmp(argv[1], "add")){
-        if(execv(EXE_DIR"add", argv) == -1)
-            perror(EXE_DIR"add");
-	}
-	else if(!strcmp(argv[1], "commit")){
-        if(execv(EXE_DIR"commit", argv) == -1)
-            perror(EXE_DIR"commit");
-	}
-	else if(!strcmp(argv[1], "diff")){
-        if(execv(EXE_DIR"diff", argv) == -1)
-            perror(EXE_DIR"diff");
-	}
-	else if(!strcmp(argv[1], "help")){
-        printf("%s", usage);
-	}
-	else if(!strcmp(argv[1], "log")){
-        if(execv(EXE_DIR"log", argv) == -1)
-            perror(EXE_DIR"log");
-	}
-	else if(!strcmp(argv[1], "reset")){
-        if(execv(EXE_DIR"reset", argv) == -1)
-            perror(EXE_DIR"reset");
-	}
-	else if(!strcmp(argv[1], "status")){
-        if(execv(EXE_DIR"status", argv) == -1)
-            perror(EXE_DIR"status");
-	}
-    else if(!strcmp(argv[1], "tag")){
-        if(execv(EXE_DIR"tag", argv) == -1)
-            perror(EXE_DIR"tag");
-	}
-	else if(!strcmp(argv[1], "version")){
-        printf("%s", version);
-	}
-	else{
-        printf("unknown option: %s\n%s", argv[1], usage);
-	}
+        case 'c':
+            switch(*cur ++){
+                case 'h':
+                    if(!strcmp(cur, "eckout")){
+                        checkDocxitPath(argv);
+                        if(execv(EXE_DIR"checkout", argv) == -1){
+                            perror(EXE_DIR"checkout");
+                            exit(0);
+                        }
+                    }
+                    else{
+                        printf("unknown option: %s\n%s", argv[1], usage);
+                        printf("the most similar command is %s\n", "checkout");
+                        exit(0);
+                    }
 
+                case 'o':
+                    switch(*cur ++){
+                        case 'm':
+                            if(!strcmp(cur, "mit")){
+                                checkDocxitPath(argv);
+                                if(execv(EXE_DIR"commit", argv) == -1){
+                                    perror(EXE_DIR"commit");
+                                    exit(0);
+                                }
+                            }
+                            else{
+                                printf("unknown option: %s\n%s", argv[1], usage);
+                                printf("the most similar command is %s\n", "commit");
+                                exit(0);
+                            }
+                        case 'n':
+                           if(!strcmp(cur, "fig")){
+                                checkDocxitPath(argv);
+                                if(execv(EXE_DIR"config", argv) == -1){
+                                    perror(EXE_DIR"config");
+                                    exit(0);
+                                }
+                            }
+                            else{
+                                printf("unknown option: %s\n%s", argv[1], usage);
+                                printf("the most similar command is %s\n", "config");
+                                exit(0);
+                            }
+                        default:
+                            printf("unknown option: %s\n%s", argv[1], usage);
+                            printf("the most similar command is %s\n", "commit config");
+                            exit(0);
+                    }
+
+                default:
+                    printf("unknown option: %s\n%s", argv[1], usage);
+                    exit(0);
+            }
+
+        case 'd':
+            if(!strcmp(cur, "iff")){
+                checkDocxitPath(argv);
+                if(execv(EXE_DIR"diff", argv) == -1){
+                    perror(EXE_DIR"diff");
+                    exit(0);
+                }
+            }
+            else{
+                printf("unknown option: %s\n%s", argv[1], usage);
+                printf("the most similar command is %s\n", "diff");
+                exit(0);
+            }
+
+        case 'h':
+            if(!strcmp(cur, "elp")){
+                printf("%s", usage);
+                exit(0);
+            }
+            else{
+                printf("unknown option: %s\n%s", argv[1], usage);
+                exit(0);
+            }
+
+        case 'i':
+            if(!strcmp(cur, "nit")){
+                if(docxitPath())
+                    printf("Nested initialization not supported\n");
+                else if(execv(EXE_DIR"init", argv) == -1)
+                    perror(EXE_DIR"init");
+                exit(0);
+            }
+            else{
+                printf("unknown option: %s\n%s", argv[1], usage);
+                printf("the most similar command is %s\n", "init");
+                exit(0);
+            }
+
+        case 'l':
+            if(!strcmp(cur, "og")){
+                checkDocxitPath(argv);
+                if(execv(EXE_DIR"log", argv) == -1){
+                    perror(EXE_DIR"log");
+                    exit(0);
+                }
+            }
+            else{
+                printf("unknown option: %s\n%s", argv[1], usage);
+                printf("the most similar command is %s\n", "log");
+                exit(0);
+            }
+
+        case 'm':
+            if(!strcmp(cur, "erge")){
+                checkDocxitPath(argv);
+                if(execv(EXE_DIR"merge", argv) == -1){
+                    perror(EXE_DIR"merge");
+                    exit(0);
+                }
+            }
+            else{
+                printf("unknown option: %s\n%s", argv[1], usage);
+                printf("the most similar command is %s\n", "merge");
+                exit(0);
+            }
+
+        case 'r':
+            if(!strcmp(cur, "eset")){
+                checkDocxitPath(argv);
+                if(execv(EXE_DIR"reset", argv) == -1){
+                    perror(EXE_DIR"reset");
+                    exit(0);
+                }
+            }
+            else{
+                printf("unknown option: %s\n%s", argv[1], usage);
+                printf("the most similar command is %s\n", "reset");
+                exit(0);
+            }
+
+        case 's':
+            if(!strcmp(cur, "tatus")){
+                checkDocxitPath(argv);
+                if(execv(EXE_DIR"status", argv) == -1){
+                    perror(EXE_DIR"status");
+                    exit(0);
+                }
+            }
+            else{
+                printf("unknown option: %s\n%s", argv[1], usage);
+                printf("the most similar command is %s\n", "status");
+                exit(0);
+            }
+
+        case 't':
+            if(!strcmp(cur, "ag")){
+                checkDocxitPath(argv);
+                if(execv(EXE_DIR"tag", argv) == -1){
+                    perror(EXE_DIR"tag");
+                    exit(0);
+                }
+            }
+            else{
+                printf("unknown option: %s\n%s", argv[1], usage);
+                printf("the most similar command is %s\n", "tag");
+                exit(0);
+            }
+
+        case 'v':
+            if(!strcmp(cur, "ersion")){
+                printf("%s", version);
+                exit(0);
+            }
+            else{
+                printf("unknown option: %s\n%s", argv[1], usage);
+                printf("the most similar command is %s\n", "version");
+                exit(0);
+            }
+
+        case '-':
+            if(*cur ++ == '-'){
+                switch(*cur ++){
+                    case 'h':
+                        if(!strcmp(cur, "elp")){
+                            printf("%s", usage);
+                            exit(0);
+                        }
+                        else{
+                            printf("unknown option: %s\n%s", argv[1], usage);
+                            exit(0);
+                        }
+
+                    case 'v':
+                        if(!strcmp(cur, "ersion")){
+                            printf("%s", version);
+                            exit(0);
+                        }
+                        else{
+                            printf("unknown option: %s\n%s", argv[1], usage);
+                            printf("the most similar command is %s\n", "--version");
+                            exit(0);
+                        }
+
+                    default:
+                        printf("unknown option: %s\n%s", argv[1], usage);
+                        exit(0);
+                }
+            }
+            else {
+                printf("unknown option: %s\n%s", argv[1], usage);
+                exit(0);
+            }
+
+        default:
+            printf("unknown option: %s\n%s", argv[1], usage);
+            exit(0);
+	}
 	return 0;
 }
