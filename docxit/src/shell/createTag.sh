6b2427746fe5a0c80fbf75128366a1914282453c
yxyyyxxyy <yixinyu@mail.ustc.edu.cn>
2018-12-06 20:58:40 +0800

    fix createTag.sh

--- docxit/src/shell/createTag.sh
+++ docxit/src/shell/createTag.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-# docxitPath, tagname, commitkey
+# docxitPath, tagname
+
+commitkey=`cat `cat $docxitPath"/.docxit/HEAD"``
 
 tfile=$1".docxit/refs/tags/"$2
 if [ -f $tfile ]
 then
 	echo "error: tag '$2' exists"
 else
-	echo -n $3 > $tfile
+	echo -n $commitkey > $tfile
 	echo "create tag '$2'"
 fi
