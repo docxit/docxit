200904d2d9de540928838f435e4b615dd4a5c2bb
2390363806 <2390363806@qq.com>
2018-12-06 21:00:03 +0800

    tag

---
+++ docxit/src/c/tag.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<string.h>
+#define EXE_DIR "/usr/local/lib/docxit/"
+
+using namespace std;
+
+#define TAGUSAGE "\
+Usage:  docxit tag                                  show all tags\n\
+        docxit tag <tag_name>                       tag current commit\n\
+        docxit tag <tag_name> <full_commit_key>     tag specified commit\n\
+        docxit tag -d <tag_name> [<tag_name> ...]   delete tags\n"
+
+int main(int argc, char *argv[])
+{
+    if(argc < 3)
+    {
+        char *argv1[] = {argv[1],NULL};
+        if(execv(EXE_DIR"showTag", argv1) == -1)
+        {
+            perror(EXE_DIR"showTag");
+            exit(0);
+        }
+    }
+    else if(argc < 4)
+    {
+        char *argv1[] = {argv[1], argv[2], NULL};
+        if(execv(EXE_DIR"createTag", argv1) == -1)
+        {
+            perror(EXE_DIR"createTag");
+            exit(0);
+        }
+    }
+    else if(strcmp(argv[2], "-d") == 0)
+    {
+        for(int i = 3; i < argc; i++)
+        {
+            char argv1[] = {argv[1], argv[i], NULL};
+            if(execv(EXE_DIR"deleteTag", argv1) == -1)
+            {
+                perror(EXE_DIR"deleteTag");
+                exit(0);
+            }
+        }
+    }
+    else
+    {
+        char *argv1[] = {argv[1], argv[2], argv[3], NULL};
+        if(execv(EXE_DIR"createTag", argv1) == -1)
+        {
+            perror(EXE_DIR"createTag");
+            exit(0);
+        }
+    }
+}
