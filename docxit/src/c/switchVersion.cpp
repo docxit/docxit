b35e4a5332f6f8f10b0ff31fdce3e66799dea877
yxyyyxxyy <yixinyu@mail.ustc.edu.cn>
2018-12-07 19:45:20 +0800

    add switch version

---
+++ docxit/src/c/switchVersion.cpp
@@ -0,0 +1,19 @@
+#include "switchVersion.h"
+
+void switchVersion(string path){
+    string indexpath = path + ".docxit/index";
+    Records rec = openIndex(indexpath.c_str());
+    for(int i = 0; i < rec.length; i ++){
+        string key = rec.base[i].key;
+        key = "cp " + path + ".docxit/object/" + key.substr(0,2) + '/' + key.substr(2,38) + " " + rec.base[i].name;
+        if(system(("rm -rf `ls " + path + " -A | grep -v .docxit`").c_str()) == -1){
+            printf("fatal: rm: cannot execute\n");
+            exit(0);
+        }
+        if(system(key.c_str()) == -1){
+            printf("fatal: %s: cannot execute\n", key,c_str());
+            exit(0);
+        }
+    }
+    freeRecords(&rec);
+}
---
+++ docxit/src/c/switchVersion.h
@@ -0,0 +1,8 @@
+#ifndef __SWITCH_VERSION_H__
+#define __SWITCH_VERSION_H__
+
+#include "indexOp.h"
+
+void switchVersion(string path);
+
+#endif // __SWITCH_VERSION_H__
